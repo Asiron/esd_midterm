2b798dd2543c49504e645254664581ff80205e99
Maciej <maciej.zurad@gmail.com>
2014-04-18 06:08:44 +0200

    Almost finished

--- esd_midterm.c
+++ esd_midterm.c
@@ -1,20 +1,27 @@
-/* helloworld.c for TOPPERS/ATK(OSEK) */ 
 #include "kernel.h"
 #include "kernel_id.h"
 #include "com_cfg.h"
 #include "ecrobot_interface.h"
-#include <stdio.h>
+#include <stdlib.h>
 
 #define LEFT_MOTOR_PORT NXT_PORT_A
 #define RIGHT_MOTOR_PORT NXT_PORT_C
 #define TOUCH_SENSOR_PORT NXT_PORT_S4
 #define LIGHT_SENSOR_PORT NXT_PORT_S1
 
-#define MIN_SPEED 57
-#define MAX_SPEED 67
+#define MIN_SPEED 59
+#define MAX_SPEED 69
+
+#define EMERGENCY_SPEED 60
+
+#define GO_BACK_ROTATIONS 2
+#define GO_OUTSIDE_ROTATIONS 2
+#define TURN_DEGREES 200
 
 DeclareCounter(SysTimerCnt);
 DeclareEvent(TouchSensorOnEvent);
+DeclareEvent(LightSensorReadingEvent);
+DeclareEvent(EmergencyEvent);
 DeclareTask(LineFollowerTask);
 DeclareTask(EmergencyTask);
 DeclareTask(MotorControlTask);
@@ -22,7 +29,28 @@ DeclareTask(TouchSensorTask);
 DeclareTask(LightSensorTask);
 DeclareTask(LCDTask);
 
+void user_1ms_isr_type2(void);
+void ecrobot_device_terminate(void);
+void ecrobot_device_initialize(void);
 void disp(int row, char* str, int val);
+void send_encoder_request();
+void send_motor_command(U32 motor, int speed, int brake);
+
+enum EMERGENCY_STATES {GO_BACK, NEEDS_TURN, TURNING, GO_AWAY}; 
+
+int touch_sensor = 0;
+int light_sensor = 0;
+
+int left_brake   = 0;
+int right_brake  = 0;
+
+int current_left_motor_speed  = 0;
+int current_right_motor_speed = 0;
+
+int initial_brightness = 0;
+
+int current_left_motor_count = 0;
+int current_right_motor_count = 0;
 
 /* nxtOSEK hook to be invoked from an ISR in category 2 */
 void user_1ms_isr_type2(void)
@@ -47,15 +75,8 @@ void ecrobot_device_terminate(void)
 	ecrobot_set_light_sensor_inactive(LIGHT_SENSOR_PORT);
 }
 
-int touch_sensor = 0;
-int light_sensor = 0;
-int left_brake   = 0;
-int right_brake  = 0;
-int current_left_motor_speed  = 0;
-int current_right_motor_speed = 0;
-int initial_brightness = 0;
 
-MOTOR_CTR_MSG	commandSnd, comandSnd2, commandRcv;
+
 
 TASK(LineFollowerTask)
 {
@@ -72,13 +93,12 @@ TASK(LineFollowerTask)
 		WaitEvent(LightSensorReadingEvent | EmergencyEvent);
 
 		EventMaskType mask = 0;
-		GetEvent(EmergencyTask, &mask);
+		GetEvent(LineFollowerTask, &mask);
 
 		if (EmergencyEvent & mask)
 		{
 			ClearEvent(EmergencyEvent);
 			TerminateTask();
-
 		} else if (LightSensorReadingEvent & mask) {
 
 			if ( newBrightness < (initial_brightness * 0.77) ) {
@@ -86,30 +106,14 @@ TASK(LineFollowerTask)
 				// Start to lose the black line
 				if ( beforeBrightness >= newBrightness ) {
 					// Turn to the right
-					commandSnd.speed = MIN_SPEED;
-					commandSnd.brake = 0;
-					commandSnd.motor = RIGHT_MOTOR_PORT;
-					SendMessage(MotorCtrMsgSnd, &commandSnd);
-
-					commandSnd.speed = MAX_SPEED;
-					commandSnd.brake = 0;
-					commandSnd.motor = LEFT_MOTOR_PORT;
-					SendMessage(MotorCtrMsgSnd, &commandSnd);
-
+					send_motor_command(RIGHT_MOTOR_PORT, MIN_SPEED, 0);
+					send_motor_command(LEFT_MOTOR_PORT, MAX_SPEED, 0);
 				}
-
 				beforeBrightness = newBrightness;
 
 			} else {
-				commandSnd.speed = MAX_SPEED;
-				commandSnd.brake = 0;
-				commandSnd.motor = RIGHT_MOTOR_PORT;
-				SendMessage(MotorCtrMsgSnd, &commandSnd);
-
-				commandSnd.speed = MIN_SPEED;
-				commandSnd.brake = 0;
-				commandSnd.motor = LEFT_MOTOR_PORT;
-				SendMessage(MotorCtrMsgSnd, &commandSnd);
+				send_motor_command(RIGHT_MOTOR_PORT, MAX_SPEED, 0);
+				send_motor_command(LEFT_MOTOR_PORT, MIN_SPEED, 0);
 			}
 			newBrightness = light_sensor;
 			ClearEvent(LightSensorReadingEvent);
@@ -137,46 +141,132 @@ TASK(TouchSensorTask)
 TASK(LightSensorTask)
 {
 	light_sensor = ecrobot_get_light_sensor(LIGHT_SENSOR_PORT);
-	SetEvent(LineFollowerTask, LightSensorReadingEvent)
+	SetEvent(LineFollowerTask, LightSensorReadingEvent);
 	TerminateTask();
 }
 
 TASK(LCDTask)
 {
 	display_clear(0);
-	
 	disp(0, "Left speed ", current_left_motor_speed);
 	disp(1, "Left brake ", left_brake);
 	disp(2, "Right speed ", current_right_motor_speed);
 	disp(3, "Right brake ", right_brake);
 	disp(4, "Light ", light_sensor);
 	disp(5, "Touch ", touch_sensor);
-	disp(6, "Second Touch ", second_touch_sensor);
-	disp(7, "Inital light ", initial_brightness);
+	disp(6, "Inital light ", initial_brightness);
 	display_update();
 	TerminateTask();
 }
 
 TASK(EmergencyTask)
 {
+	WaitEvent(TouchSensorOnEvent);
+	ClearEvent(TouchSensorOnEvent);
+
 	SetEvent(LineFollowerTask, EmergencyEvent);
+
+	send_motor_command(LEFT_MOTOR_PORT, 0, 0);
+	send_motor_command(RIGHT_MOTOR_PORT, 0, 0);
+
+	int intial_left_count  = 0;
+	int intial_right_count = 0;
+
+	int terminate = 0;
+
+	int state = GO_BACK;
+
+	while(!terminate) {
+
+		send_encoder_request();
+
+		switch(state) {
+
+			case GO_BACK:
+			{
+				send_motor_command(LEFT_MOTOR_PORT, -EMERGENCY_SPEED, 0);
+				send_motor_command(RIGHT_MOTOR_PORT, -EMERGENCY_SPEED, 0);
+
+				if (abs(current_right_motor_count - intial_right_count) / 360 >= GO_BACK_ROTATIONS) {
+					state = NEEDS_TURN;
+				}
+				break;
+			}
+			case NEEDS_TURN:
+			{
+				send_motor_command(LEFT_MOTOR_PORT, EMERGENCY_SPEED, 0);
+				state = TURNING;
+
+				intial_left_count  = current_right_motor_count;
+				intial_right_count = current_right_motor_count;
+				break;
+			}
+			case TURNING:
+			{
+				if (abs(current_right_motor_count - intial_right_count) >= TURN_DEGREES) {
+					state = GO_AWAY;
+					intial_right_count = current_right_motor_count;
+				}
+				break;
+			}
+			case GO_AWAY:
+			{
+				send_motor_command(LEFT_MOTOR_PORT, EMERGENCY_SPEED, 0);
+				send_motor_command(RIGHT_MOTOR_PORT, EMERGENCY_SPEED, 0);
+
+				if (abs(current_right_motor_count - intial_right_count) / 360 >= GO_OUTSIDE_ROTATIONS) {
+					terminate = 1;
+				}
+				break;
+			}
+		}
+	}
+
+	send_motor_command(LEFT_MOTOR_PORT, 0, 0);
+	send_motor_command(RIGHT_MOTOR_PORT, 0, 0);
+
 	TerminateTask();
 }
 
 TASK(MotorControlTask)
 {
-	RecieveMessage(MotorCtrMsgRcv,&commandRcv);
+	MOTOR_CTR_MSG message;
 
-	nxt_motor_set_speed(commandRcv.motor, commandRcv.speed, commandRcv.brake);
+	RecieveMessage(MotorCtrMsgRcv,&message);
 
-	if (commandRcv.motor == RIGHT_MOTOR_PORT) {
-		current_right_motor_speed = commandRcv.speed;
-		right_brake = commandRcv.brake;
-	} else if (commandRcv.motor == LEFT_MOTOR_PORT) {
-		current_left_motor_speed  = commandRcv.speed;
-		left_brake  = commandRcv.brake;
+	if (message.encoder_request)
+	{
+		current_left_motor_count  = nxt_motor_get_count(LEFT_MOTOR_PORT);
+		current_right_motor_count = nxt_motor_get_count(RIGHT_MOTOR_PORT);
+		TerminateTask();
+	}
+
+	nxt_motor_set_speed(message.motor, message.speed, message.brake);
+
+	if (message.motor == RIGHT_MOTOR_PORT) {
+		current_right_motor_speed = message.speed;
+		right_brake = message.brake;
+	} else if (message.motor == LEFT_MOTOR_PORT) {
+		current_left_motor_speed  = message.speed;
+		left_brake  = message.brake;
 	}
 	TerminateTask();
+}
+
+void send_encoder_request()
+{
+	MOTOR_CTR_MSG message;
+	message.encoder_request = 1;
+	SendMessage(MotorCtrMsgSnd, &message);
+}
+
+void send_motor_command(U32 motor, int speed, int brake){
+	MOTOR_CTR_MSG message;
+	message.motor = motor;
+	message.speed = speed;
+	message.brake = brake;
+	message.encoder_request = 0;
+	SendMessage(MotorCtrMsgSnd, &message);
 }
 
 void disp(int row, char *str, int val)
